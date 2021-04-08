e2b598231711c99eea47929d9c876f4e576bb0aa
Alden Fachetti <aldenmerlin@gmail.com>
2021-04-08 18:46:57 -0300

    first commit - setting up strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '264cc3cab3105b83730120c772d058c3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fa3dc58a-1672-4e99-84b9-626df8fcab98'
+};
